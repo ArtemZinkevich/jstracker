0a10b1518fec9134d6c428e918fb89e5d4e52f67
Nikita Beloglazov <nikelandjelo@gmail.com>
2017-02-05 18:17:52 -0800

    Create initial mavent project structure.
    The project is going to have 2 modules right now: site and server. Site contains only html/js/css while server is java server.

---
+++ server/src/io/jstracker/server/SampleClass.java
@@ -0,0 +1,11 @@
+package io.jstracker.server;
+
+/**
+ * Sample class so that initial project is not empty. Delete once we have real classes.
+ */
+public class SampleClass {
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
+}
